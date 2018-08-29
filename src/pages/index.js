2c76f1f11f32bbeeb952191dd18e16bc84728445
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2018-08-28 18:50:13 -0700

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi worldly people</h1>
+    <p>I'm Blane Downing-Jennings</p>
+  </div>
+)
+
+export default IndexPage
